1d908d2a6777ad7f42b52920d989f1f0bbb9779f
Hongyang Lyu <s3694521@student.rmit.edu.au>
2019-07-27 12:13:39 +0000

    Created a home page and Route settings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+    
+  # According to the page, return the correct title
+  def full_title(page_title = '')
+    base_title = "Car Sharing"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+  
+
+
 end
